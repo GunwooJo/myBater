64149acd3c40e6495a73f2466137ad74020ac215
GunwooJo <kanggi1997@naver.com>
2023-09-24 03:29:06 +0900

    db에 저장된 비번과 로그인폼에 입력한 비번 비교하기

--- routes/user.js
+++ routes/user.js
@@ -34,6 +34,43 @@ router.post('/register', async (req, res)=>{
   dbConnection.end();
 })
 
+router.post('/login', async (req, res)=>{
+  try {
+    const body = {...req.body};
+
+    let sql = 'SELECT * FROM user WHERE id = ?';
+    let params = [body.id];
+
+    dbConnection.connect();
+
+    dbConnection.query(sql, params, (error, result)=>{
+      if(error) {
+        console.error('Error executing the query: '+ error.stack);
+        res.sendStatus(500);
+      }
+      else {
+        const user = result[0];
+        dbConnection.end();
+        
+        //If user not found or password does not match, send error response
+        if(!user || !bcrypt.compareSync(body.pw, user.pw)) {
+          return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // Here you would typically create a token or a session and send it to the client
+        // For simplicity, we're just sending a success message
+        res.json({ message: 'Login successful!' });
+        console.log('로그인 성공');
+      }
+
+    })
+    
+  } catch (error) {
+    console.error('Error handling login request:', error.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+  
+})
 
 
 module.exports = router;
