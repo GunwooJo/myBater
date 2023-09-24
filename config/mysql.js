46dc19fcb7594ed5d844be20d87cf58fbdb0cff0
GunwooJo <kanggi1997@naver.com>
2023-09-25 01:27:23 +0900

    커넥션 풀 구현완료

--- config/mysql.js
+++ config/mysql.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 const mysql = require("mysql2");
 
-let dbInfo = {
-  host: process.env.DB_HOST, // 데이터베이스 주소
-  port: "3306", // 데이터베이스 포트
-  user: process.env.DB_USER, // 로그인 계정
-  password: process.env.DB_PASSWORD, // 비밀번호
-  database: "lets", // 엑세스할 데이터베이스
-};
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 5
+})
 
-let dbConnection = mysql.createConnection(dbInfo);
-
-module.exports = dbConnection;
+module.exports = pool;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const dbConnection = require("../config/mysql");
+const pool = require("../config/mysql");
 const bcrypt = require('bcrypt');
 
 //회원가입 요청 처리
@@ -15,24 +15,24 @@ router.post('/register', async (req, res)=>{
     let sql = 'INSERT INTO user(id,pw,nickname,email,region) VALUES(?,?,?,?,?)'
     let params = [body.id, hashedPassword, body.nickname, body.email, body.region]
 
-    dbConnection.connect();
-
-    dbConnection.query(sql, params, (error)=>{
-      if(error) {
-        console.error('Error executing the query: '+ error.stack);
-        res.sendStatus(500);
-      }
-      else {
-        res.sendStatus(200)
-      }
-
+    pool.getConnection((error, connection)=>{
+      connection.query(sql, params, (error)=>{
+        if(error) {
+          console.error('Error executing the query: '+ error.stack);
+          res.sendStatus(500);
+        }
+        else {
+          res.sendStatus(200)
+          connection.release();
+        }
+  
+      })
     })
-    
+
   } catch (error) {
     console.error(error);
     res.status(500).send('DB저장 실패.');
   }
-  dbConnection.end();
 })
 
 //로그인 요청 처리
@@ -43,29 +43,29 @@ router.post('/login', async (req, res)=>{
     let sql = 'SELECT * FROM user WHERE id = ?';
     let params = [body.id];
 
-    dbConnection.connect();
-
-    dbConnection.query(sql, params, (error, result)=>{
-      if(error) {
-        console.error('Error executing the query: '+ error.stack);
-        res.sendStatus(500);
-      }
-      else {
-        const user = result[0];
-        dbConnection.end();
-        
-        //If user not found or password does not match, send error response
-        if(!user || !bcrypt.compareSync(body.pw, user.pw)) {
-          return res.status(401).json({ error: 'Invalid username or password' });
+    pool.getConnection((error, connection)=>{
+      connection.query(sql, params, (error, result)=>{
+        if(error) {
+          console.error('Error executing the query: '+ error.stack);
+          res.sendStatus(500);
         }
-
-        // Here you would typically create a token or a session and send it to the client
-        // For simplicity, we're just sending a success message
-        res.json({ message: 'Login successful!' });
-        console.log('로그인 성공');
-      }
-
+        else {
+          const user = result[0];
+          
+          //If user not found or password does not match, send error response
+          if(!user || !bcrypt.compareSync(body.pw, user.pw)) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+          }
+  
+          // Here you would typically create a token or a session and send it to the client
+          // For simplicity, we're just sending a success message
+          res.json({ message: 'Login successful!' });
+          console.log('로그인 성공');
+        }
+  
+      })
     })
+    
     
   } catch (error) {
     console.error('Error handling login request:', error.stack);
