ab10e1359e0e24cc56683f6c3ed7a2fbac6c65a6
GunwooJo <kanggi1997@naver.com>
2023-09-25 02:12:12 +0900

    수정: 커넥션 리밋 5 -> 10

--- config/mysql.js
+++ config/mysql.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: 5
+  connectionLimit: 10
 })
 
 module.exports = pool;
